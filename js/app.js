33649d7d645236cbb783d27c6b0c24b8f7ddcaa9
Juan Esquivias <juanesquiviasotazu@gmail.com>
2016-10-09 14:38:51 -0500

    modals in form

--- js/app.js
+++ js/app.js
@@ -2,22 +2,67 @@ const $form = $('#suscribeForm')
 const $formGroup = $form.find('.form-group')
 const $formControl = $formGroup.find('.form-control')
 
-$form.submit(function (ev) {
+$form.submit((ev) => {
   ev.preventDefault()
-
+  
   const error = Math.random() > 0.5
   $formGroup.removeClass().addClass('form-group')
   $formControl.removeClass().addClass('form-control')
-  $formGroup.addClass( error ? 'has-danger' : 'has-success')
-  $formControl.addClass( error ? 'form-control-danger' : 'form-control-success')
-  $formGroup.find('form-control-feedback').remove()
-
+  $formGroup.addClass(error ? 'has-danger' : 'has-success')
+  $formControl.addClass(error ? 'form-control-danger' : 'form-control-success')
+  $formGroup.find('.form-control-feedback').remove()
   var el;
   if (error) {
-    el = $('<div>', { html: 'An error has occurred' })
-  }else {
-    el = $('<div>', { html: 'We will send all the news' })
+    el = $('<div>', { html: 'Ha ocurrido un error' })
+  } else {
+    el = $('<div>', { html: 'Te enviaremos todas las novedades!' })
   }
   el.addClass('form-control-feedback')
   $formGroup.append(el)
 })
+
+function mostrarModal() {
+  const noMostrarModal = JSON.parse(localStorage.noMostrarModal)
+  if (!noMostrarModal) {
+    $('#modalOferta').modal()
+  }
+
+  $('#btnNoRegistrar').click((ev) => {
+    localStorage.noMostrarModal = true
+  })
+}
+
+// const $form = $('#suscribeForm')
+// const $formGroup = $form.find('.form-group')
+// const $formControl = $formGroup.find('.form-control')
+
+// $form.submit(function (ev) => {
+//   ev.preventDefault()
+
+//   const error = Math.random() > 0.5
+//   $formGroup.removeClass().addClass('form-group')
+//   $formControl.removeClass().addClass('form-control')
+//   $formGroup.addClass( error ? 'has-danger' : 'has-success')
+//   $formControl.addClass( error ? 'form-control-danger' : 'form-control-success')
+//   $formGroup.find('form-control-feedback').remove()
+
+//   var el;
+//   if (error) {
+//     el = $('<div>', { html: 'An error has occurred' })
+//   }else {
+//     el = $('<div>', { html: 'We will send all the news' })
+//   }
+//   el.addClass('form-control-feedback')
+//   $formGroup.append(el)
+// })
+
+// function mostrarModal() {
+//   const noMostrarModal = JSON.parse(localStorage.noMostrarModal)
+//   if (!noMostrarModal) {
+//     $('#modalOferta').modal()
+//   }
+
+//   $('#btnNoRegistrar').click(function (ev) {
+//     localStorage.noMostrarModal = true
+//   })
+// }
