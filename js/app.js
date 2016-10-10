df9a488b7bf739f2c534e738f14b3b6709ee8ae9
Juan Esquivias <juanesquiviasotazu@gmail.com>
2016-10-10 11:41:48 -0500

    add filters for products and functionality with javascript

--- js/app.js
+++ js/app.js
@@ -32,6 +32,20 @@ function mostrarModal() {
   })
 }
 
+const $filtrosToggle = $('#filtrosToggle')
+$filtrosToggle.click(function (ev) {
+  ev.preventDefault()
+
+  const $i = $filtrosToggle.find('i.fa')
+  const isDown = $i.hasClass('fa-chevron-down')
+  if (isDown) {
+    $i.removeClass('fa-chevron-down').addClass('fa-chevron-up')
+  } else {
+    $i.removeClass('fa-chevron-up').addClass('fa-chevron-down')
+  }
+})
+
+
 // const $form = $('#suscribeForm')
 // const $formGroup = $form.find('.form-group')
 // const $formControl = $formGroup.find('.form-control')
